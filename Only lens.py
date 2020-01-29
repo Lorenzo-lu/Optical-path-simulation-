e7e752db22c5918447a6a4b1b5e1c4ae65714852
Yizhou~Lu <ylu289@wisc.edu>
2020-01-29 08:47:04 -0600

    Update Only lens.py

--- Only lens.py
+++ Only lens.py
@@ -1,21 +1,26 @@
+import numpy as np;
+import matplotlib.pyplot as plt;
+
+print('running');
+
+
 class lens:
     def __init__(self,x,f):
         self.posit = x;
         self.focus = x + f;
+        # minus f if concave lens; plus f if convex lens
         
-import numpy as np;
-import matplotlib.pyplot as plt;
 
 class beams:
     def __init__(self,origin,direction,lens):
         self.x = [origin[0]];
         self.y = [origin[1]];
-        self.lens = lens;
+        self.lens = lens; 
         self.direction = direction;
         
         for L in lens:            
             self.nodes_at_len(L);
-            self.nodes_converge(L);
+            #self.nodes_converge(L);
         
     def nodes_at_len(self,Len):
         # nodes at the len
@@ -23,15 +28,23 @@ class beams:
         y = (x - self.x[-1]) / self.direction[0] * self.direction[1] + self.y[-1];
         self.x += [x];
         self.y += [y];
+        self.nodes_converge(Len);
         
         
     def nodes_converge(self,Len):
+        # this function is called by the last one
+        # don't worry
         
         focus = [Len.focus,0];
         center = [Len.posit,0];
-        posi0 = [self.x[-2],self.y[-2]];
-        posi1 = [Len.posit,self.y[-2]];
-       
+        posi0 = [self.x[-2],self.y[-2]];# from the previous len
+        posi1 = [Len.posit,self.y[-2]]; # on current len
+
+        if posi0[1] == center[1]:
+            # when posi0 and the focus of this len overlap
+            # it means d1 = 2f
+            self.direction[1] *= -1;
+            return;       
 
         M = np.array([[posi0[0],1],[center[0],1]]);
         z = np.array([[posi0[1]],[center[1]]]);
@@ -54,6 +67,12 @@ class beams:
         # calculate the intersection of these two beams
         M = np.array([[-k1[0,0],1],[-k2[0,0],1]]);
         z = np.array([[k1[1,0]],[k2[1,0]]]);
+
+        if np.linalg.matrix_rank(M) <2:
+            delta_x = focus[0] - posi1[0];
+            delta_y = focus[1] - posi1[1];
+            self.direction = [delta_x,delta_y];
+            return;
         
         inv_M = np.linalg.pinv(M);
         
@@ -70,12 +89,15 @@ class beams:
 L1 = lens(10,5);
 L2 = lens(20,5);
 L3 = lens(30,5);
-L4 = lens(40,5);
+L4 = lens(45,5);
 
-B1 = beams([0,5],[1,0],[L1,L2,L3])
-B2 = beams([0,5],[10,-4.999],[L1,L2,L3]); # with small bug here (singual value)
+B1 = beams([0,5],[10,0],[L1,L2,L3])
+B2 = beams([0,5],[10,-5],[L1,L2,L3]); # with small bug here (singular value)
 #print(B.x,B.y)
+print(B1.x,B1.y);
+print(B2.x,B2.y);
 
 plt.figure();
 plt.plot(B1.x,B1.y);
 plt.plot(B2.x,B2.y);
+plt.show();
